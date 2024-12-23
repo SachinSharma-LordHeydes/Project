3a5379f9851035e26a94fd8679770b768ca05fd0
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2024-12-23 20:29:35 +0545

    ap1/v1 added

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,82 +2,82 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",
-  SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
-  SENDRESETPASSMAIL_API: BASE_URL + "/auth/send-reset-password-mail",
-  RESETPASSWORD_API: BASE_URL + "/auth/reset-password-token",
+  SENDOTP_API: BASE_URL + "/api/v1/auth/sendotp",
+  SIGNUP_API: BASE_URL + "/api/v1/auth/signup",
+  LOGIN_API: BASE_URL + "/api/v1/auth/login",
+  SENDRESETPASSMAIL_API: BASE_URL + "/api/v1/auth/send-reset-password-mail",
+  RESETPASSWORD_API: BASE_URL + "/api/v1/auth/reset-password-token",
 }
 
 // PROFILE ENDPOINTS
 export const profileEndpoints = {
-  GET_USER_DETAILS_API: BASE_URL + "/profile/getUserDetails",
-  GET_PROFILE_DETAILS_API: BASE_URL + "/profile/getUserProfile",
-  GET_USER_ENROLLED_COURSES_API: BASE_URL + "/profile/getEnrolledCourses",
-  GET_INSTRUCTOR_DATA_API: BASE_URL + "/profile/instructorDashboard",
+  GET_USER_DETAILS_API: BASE_URL + "/api/v1/profile/getUserDetails",
+  GET_PROFILE_DETAILS_API: BASE_URL + "/api/v1/profile/getUserProfile",
+  GET_USER_ENROLLED_COURSES_API: BASE_URL + "/api/v1/profile/getEnrolledCourses",
+  GET_INSTRUCTOR_DATA_API: BASE_URL + "/api/v1/profile/instructorDashboard",
 }
 
 // STUDENTS ENDPOINTS
 export const studentEndpoints = {
-  COURSE_INITILIZE_PAYMENT_API: BASE_URL + "/payment/initialize-esewa",
-  COURSE_COMPLETE_PAYMENT_API: BASE_URL + "/payment/complete-payment",
-  SEND_PAYMENT_SUCCESS_EMAIL_API: BASE_URL + "/payment/sendPaymentSuccessEmail",
+  COURSE_INITILIZE_PAYMENT_API: BASE_URL + "/api/v1/payment/initialize-esewa",
+  COURSE_COMPLETE_PAYMENT_API: BASE_URL + "/api/v1/payment/complete-payment",
+  SEND_PAYMENT_SUCCESS_EMAIL_API: BASE_URL + "/api/v1/payment/sendPaymentSuccessEmail",
 }
 
 // COURSE ENDPOINTS
 export const courseEndpoints = {
-  GET_ALL_COURSE_API: BASE_URL + "/course/getAllCourses",
-  COURSE_DETAILS_API: BASE_URL + "/course/getCourseDetails",
-  EDIT_COURSE_API: BASE_URL + "/course/editCourse",
-  COURSE_CATEGORIES_API: BASE_URL + "/course/showAllCategories",
-  CREATE_COURSE_API: BASE_URL + "/course/createCourse",
-  CREATE_SECTION_API: BASE_URL + "/course/addSection",
-  GET_SECTION_API: BASE_URL + "/course/getSection",
-  GET_ALL_SECTION_API: BASE_URL + "/course/getAllSection",
+  GET_ALL_COURSE_API: BASE_URL + "/api/v1/course/getAllCourses",
+  COURSE_DETAILS_API: BASE_URL + "/api/v1/course/getCourseDetails",
+  EDIT_COURSE_API: BASE_URL + "/api/v1/course/editCourse",
+  COURSE_CATEGORIES_API: BASE_URL + "/api/v1/course/showAllCategories",
+  CREATE_COURSE_API: BASE_URL + "/api/v1/course/createCourse",
+  CREATE_SECTION_API: BASE_URL + "/api/v1/course/addSection",
+  GET_SECTION_API: BASE_URL + "/api/v1/course/getSection",
+  GET_ALL_SECTION_API: BASE_URL + "/api/v1/course/getAllSection",
   
-  ADD_CART_API:BASE_URL + "/course/addToCart",
-  REMOVE_CART_API:BASE_URL + "/course/removeFromTheCart",
+  ADD_CART_API:BASE_URL + "/api/v1/course/addToCart",
+  REMOVE_CART_API:BASE_URL + "/api/v1/course/removeFromTheCart",
   
-  GET_SUBSECTION_API: BASE_URL + "/course/getSubSectionHandler",
-  CREATE_SUBSECTION_API: BASE_URL + "/course/addSubSection",
-  UPDATE_SECTION_API: BASE_URL + "/course/updateSection",
-  UPDATE_SUBSECTION_API: BASE_URL + "/course/updateSubSection",
-  GET_ALL_INSTRUCTOR_COURSES_API: BASE_URL + "/course/getInstructorCourses",
-  DELETE_SECTION_API: BASE_URL + "/course/deleteSection",
-  DELETE_SUBSECTION_API: BASE_URL + "/course/deleteSubSection",
-  DELETE_COURSE_API: BASE_URL + "/course/deleteCourse",
-  GET_FULL_COURSE_DETAILS_AUTHENTICATED: BASE_URL + "/course/getFullCourseDetails",
-  LECTURE_COMPLETION_API: BASE_URL + "/course/updateCourseProgress",
-  CREATE_RATING_API: BASE_URL + "/course/createRating",
-  ADD_COURSESTATUS_API: BASE_URL + "/course/addCourseStatusHandler",
-  GET_COURSE_BY_CATAGORY: BASE_URL + "/course/getCourseByCatagoryHandler",
+  GET_SUBSECTION_API: BASE_URL + "/api/v1/course/getSubSectionHandler",
+  CREATE_SUBSECTION_API: BASE_URL + "/api/v1/course/addSubSection",
+  UPDATE_SECTION_API: BASE_URL + "/api/v1/course/updateSection",
+  UPDATE_SUBSECTION_API: BASE_URL + "/api/v1/course/updateSubSection",
+  GET_ALL_INSTRUCTOR_COURSES_API: BASE_URL + "/api/v1/course/getInstructorCourses",
+  DELETE_SECTION_API: BASE_URL + "/api/v1/course/deleteSection",
+  DELETE_SUBSECTION_API: BASE_URL + "/api/v1/course/deleteSubSection",
+  DELETE_COURSE_API: BASE_URL + "/api/v1/course/deleteCourse",
+  GET_FULL_COURSE_DETAILS_AUTHENTICATED: BASE_URL + "/api/v1/course/getFullCourseDetails",
+  LECTURE_COMPLETION_API: BASE_URL + "/api/v1/course/updateCourseProgress",
+  CREATE_RATING_API: BASE_URL + "/api/v1/course/createRating",
+  ADD_COURSESTATUS_API: BASE_URL + "/api/v1/course/addCourseStatusHandler",
+  GET_COURSE_BY_CATAGORY: BASE_URL + "/api/v1/course/getCourseByCatagoryHandler",
 }
 
 // RATINGS AND REVIEWS
 export const ratingsEndpoints = {
-  REVIEWS_DETAILS_API: BASE_URL + "/course/getReviews",
+  REVIEWS_DETAILS_API: BASE_URL + "/api/v1/course/getReviews",
 }
 
 // CATAGORIES API
 export const categories = {
-  CATEGORIES_API: BASE_URL + "/course/showAllCategories",
-  GET_CLICKED_CATEGORIES_API: BASE_URL + "/course/getClickedCatagory",
+  CATEGORIES_API: BASE_URL + "/api/v1/course/showAllCategories",
+  GET_CLICKED_CATEGORIES_API: BASE_URL + "/api/v1/course/getClickedCatagory",
 }
 
 // CATALOG PAGE DATA
 export const catalogData = {
-  CATALOGPAGEDATA_API: BASE_URL + "/course/getCategoryPageDetails",
+  CATALOGPAGEDATA_API: BASE_URL + "/api/v1/course/getCategoryPageDetails",
 }
 // CONTACT-US API
 export const contactusEndpoint = {
-  CONTACT_US_API: BASE_URL + "/reach/contact",
+  CONTACT_US_API: BASE_URL + "/api/v1/reach/contact",
 }
 
 // SETTINGS PAGE API
 export const settingsEndpoints = {
-  UPDATE_DISPLAY_PICTURE_API: BASE_URL + "/profile/updateDisplayPicture",
-  UPDATE_PROFILE_API: BASE_URL + "/profile/updateProfile",
-  UPDATE_PROFILE_PICTURE_API: BASE_URL + "/profile/updateProfilePicture",
-  CHANGE_PASSWORD_API: BASE_URL + "/auth/changepassword",
-  DELETE_PROFILE_API: BASE_URL + "/profile/deleteProfile",
+  UPDATE_DISPLAY_PICTURE_API: BASE_URL + "/api/v1/profile/updateDisplayPicture",
+  UPDATE_PROFILE_API: BASE_URL + "/api/v1/profile/updateProfile",
+  UPDATE_PROFILE_PICTURE_API: BASE_URL + "/api/v1/profile/updateProfilePicture",
+  CHANGE_PASSWORD_API: BASE_URL + "/api/v1/auth/changepassword",
+  DELETE_PROFILE_API: BASE_URL + "/api/v1/profile/deleteProfile",
 }
