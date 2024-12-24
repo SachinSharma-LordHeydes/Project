b23d26c60bd1f4635cdbe67dab01777d5771440a
SachinSharma-LordHeydes <thereisamailforme@gmail.com>
2024-12-24 12:13:26 +0545

    Update userModel.js

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -40,7 +40,6 @@ const userSchema = new mongoose.Schema({
   cart:[{
     type:mongoose.Schema.Types.ObjectId,
     ref:'courseModel',
-    unique:true,
   }],
   courseProgress: [{
     type: mongoose.Schema.Types.ObjectId,
